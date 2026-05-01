f42480dc7cde86a737e19ca747ecf5ad1dbd8a8d
LuizGenz <genzlewis@gmail.com>
2026-05-01 14:28:15 -0700

    api

---
+++ api/update.js
@@ -0,0 +1,48 @@
+function compareVersions(current, target) {
+  const a = current.split(".").map(Number);
+  const b = target.split(".").map(Number);
+
+  const len = Math.max(a.length, b.length);
+
+  for (let i = 0; i < len; i++) {
+    const x = a[i] || 0;
+    const y = b[i] || 0;
+
+    if (x < y) return -1;
+    if (x > y) return 1;
+  }
+
+  return 0;
+}
+
+export default function handler(req, res) {
+  try {
+    const config = {
+      latestVersion: "1.0.1",
+      minimumVersion: "1.0.0",
+      updateUrl: "https://smartmarkapks.vercel.app/",
+      message: "New features + bug fixes 🔥",
+    };
+
+    const currentVersion = req.query.version || "0.0.0";
+
+    const isUpdateAvailable =
+      compareVersions(currentVersion, config.latestVersion) === -1;
+
+    const isForceUpdate =
+      compareVersions(currentVersion, config.minimumVersion) === -1;
+
+    res.status(200).json({
+      ...config,
+      isUpdateAvailable,
+      isForceUpdate,
+      currentVersion,
+    });
+  } catch (error) {
+
+    res.status(500).json({
+      error: "Server crashed",
+      message: error.message,
+    });
+  }
+}
